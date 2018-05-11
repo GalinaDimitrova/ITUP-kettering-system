37c3cac481d38fe9e2e1618a042e5b4b01a077a9
GalinaDimitrova <gdimitrova@nemetschek.bg>
2018-05-11 19:02:21 +0300

    Register all created until now models to the Django Admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,48 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+# from django.contrib.auth.forms import UserChangeForm
 
-# Register your models here.
+from accounts.models import User, Office
+
+
+# TODO: Add User form and hide the password. Add Change form that contains only fields 'username', 'password1' and 'password2'
+class UserAdmin(admin.ModelAdmin):
+    # form = UserChangeForm
+    fieldsets = (
+        (None, {'fields': ('uid', 'username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'office')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    filter_horizontal = ['user_permissions', 'groups']
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'office')
+    ordering = ('username',)
+    raw_id_fields = ()
+
+admin.site.register(User, UserAdmin)
+
+
+class OfficeForm(forms.ModelForm):
+    class Meta:
+        model = Office
+        fields = '__all__'
+        widgets = {
+            'address': forms.Textarea(attrs={'cols': 80, 'rows': 5}),
+        }
+
+
+class OfficeAdmin(admin.ModelAdmin):
+    form = OfficeForm
+admin.site.register(Office, OfficeAdmin)
+
+
+class PermissionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Permission, PermissionAdmin)
--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,60 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import BaseMenu, LunchMenu, Comment, Order, MenuItem
+
+
+class MenuItemForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': 80, 'rows': 7}),
+        }
+
+
+class ManuItemsInline(admin.StackedInline):
+    model = MenuItem
+    form = MenuItemForm
+
+
+class BaseMenuAdmin(admin.ModelAdmin):
+    inlines = [
+        ManuItemsInline,
+    ]
+admin.site.register(BaseMenu, BaseMenuAdmin)
+
+
+class LunchMenuAdmin(admin.ModelAdmin):
+    filter_horizontal = ['menu_items']
+admin.site.register(LunchMenu, LunchMenuAdmin)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        widgets = {
+            'text': forms.Textarea(attrs={'cols': 80, 'rows': 7}),
+        }
+
+
+class CommentAdmin(admin.ModelAdmin):
+    form = CommentForm
+    raw_id_fields = ('user', 'menu_item')
+
+admin.site.register(Comment, CommentAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+    filter_horizontal = ['menu_items']
+admin.site.register(Order, OrderAdmin)
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    form = MenuItemForm
+admin.site.register(MenuItem, MenuItemAdmin)
